50e7511b4a4afc27bd4e6ae3ce1aa5bec0c73e9f
dnh2000 <danielhildebrandt@live.com>
2024-02-20 06:17:00 -0500

    Update mxpfu-nodejsLabs

--- index_withauth.js
+++ index_withauth.js
@@ -6,29 +6,27 @@ const session = require('express-session')
 const app = express();
 const PORT =5000;
 
-app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
+app.use(session({secret:"fingerprint",resave: true, saveUninitialized: true}))
 
 app.use(express.json());
 
 app.use("/user", (req,res,next)=>{
-// Middleware which tells that the user is authenticated or not
-   
-   if(req.session.authorization) {
-       let token = req.session.authorization['accessToken']; // Access Token
-       
-       jwt.verify(token, "access",(err,user)=>{
-           if(!err){
-               req.user = user;
-               next();
-           }
-           else{
-               return res.status(403).json({message: "User not authenticated"})
-           }
-        });
-    } else {
-        return res.status(403).json({message: "User not logged in"})
-    }
-});
+    // Middleware which tells that the user is authenticated or not
+       if(req.session.authorization) {
+           let token = req.session.authorization['accessToken']; // Access Token
+           jwt.verify(token, "access",(err,user)=>{
+               if(!err){
+                   req.user = user;
+                   next();
+               }
+               else{
+                   return res.status(403).json({message: "User not authenticated"})
+               }
+            });
+        } else {
+            return res.status(403).json({message: "User not logged in"})
+        }
+    });
 
 app.use("/user", routes);
 
@@ -40,7 +38,6 @@ app.post("/login", (req,res) => {
     let accessToken = jwt.sign({
         data: user
       }, 'access', { expiresIn: 60 * 60 });
-
       req.session.authorization = {
         accessToken
     }
